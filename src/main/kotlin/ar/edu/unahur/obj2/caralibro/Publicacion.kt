254143bbc006f5a7a6ba23a21c84745deb00b07e
Daniel Mendez <rdmmendez@hotmail.com>
2021-04-21 00:32:03 -0300

    Modificaciones necesarias para que los test funcionen

--- src/main/kotlin/ar/edu/unahur/obj2/caralibro/Publicacion.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/caralibro/Publicacion.kt
@@ -5,13 +5,28 @@ import kotlin.math.roundToInt
 
 abstract class Publicacion {
     abstract fun espacioQueOcupa(): Int
+    abstract fun meGusta()
+    abstract fun cantidadDeMeGusta(): Int
+
 }
 
 class Foto(val alto: Int, val ancho: Int) : Publicacion() {
     val factorDeCompresion = 0.7
+    var cantidadDeMeGusta = 0
+
     override fun espacioQueOcupa() = ceil(alto * ancho * factorDeCompresion).toInt()
+    override fun meGusta() {
+        cantidadDeMeGusta += 1
+    }
+    override fun cantidadDeMeGusta() = cantidadDeMeGusta
 }
 
 class Texto(val contenido: String) : Publicacion() {
+    var cantidadDeMeGusta = 0
+
     override fun espacioQueOcupa() = contenido.length
+    override fun meGusta() {
+        cantidadDeMeGusta += 1
+    }
+    override fun cantidadDeMeGusta() = cantidadDeMeGusta
 }
