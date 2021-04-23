f2213e3f50df36b06cd7198d0d7d76d868ecc18e
Daniel Mendez <rdmmendez@hotmail.com>
2021-04-23 08:27:02 -0300

    Modificaciones en las publicaciones

--- src/main/kotlin/ar/edu/unahur/obj2/caralibro/Publicacion.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/caralibro/Publicacion.kt
@@ -1,22 +1,24 @@
 package ar.edu.unahur.obj2.caralibro
 
-import java.time.Duration
+import java.security.KeyStore
 import kotlin.math.ceil
-import kotlin.reflect.jvm.internal.impl.serialization.deserialization.FlexibleTypeDeserializer
 
 
 //--------------------------------------------PUBLICACION-BASE---/
-abstract class Publicacion(usuario: Usuario) {
-    abstract fun espacioQueOcupa(): Int
-    lateinit var permisoPublicacion: Permiso
+abstract class Publicacion() {
     var usuariosDeMeGusta = mutableListOf<Usuario>()
-    fun cambiarPermiso(nuevoPermiso: Permiso){
+    var permisoPublicacion: String = "Publico"
+
+    abstract fun espacioQueOcupa(): Int
+
+    fun cambiarPermiso(nuevoPermiso: String){
         permisoPublicacion = nuevoPermiso
     }
+    fun permisoPublicacion(): String = permisoPublicacion
 
-    fun admiteAUsuario(usuario: Usuario) = permisoPublicacion.contieneAUsuario(usuario)
-
+/*    fun admiteAUsuario(usuario: Usuario) = permisoPublicacion.contieneAUsuario(usuario)
     fun recibirMeGusta(usuario: Usuario){
+        //usar check
         if (!(usuariosDeMeGusta.contains(usuario))){
             usuariosDeMeGusta.add(usuario)
         }
@@ -24,25 +26,11 @@ abstract class Publicacion(usuario: Usuario) {
             throw Exception("El usuario ya ha dado like.")
         }
     }
-
     fun cantidadDeMeGusta() = usuariosDeMeGusta.size
+
+ */
 }
 
-//----------------------------------PERMISOS-------------------/
-abstract class Permiso {
-    abstract fun quienPuedeVer(publicacion: Publicacion): List<Usuario>
-    fun contieneAUsuario(usuario: Usuario) = this.quienPuedeVer().contains(usuario)
-}
-
-//----------------------permisitos---/
-class publico(): Permiso(){
-    override fun quienPuedeVer(publicacion: Publicacion): List<Usuario> {
-
-    }
-}
-class soloAmigos(): Permiso(){}
-class privadoConPermitidos(): Permiso(){}
-class privadoConExcluidos(): Permiso(){}
 
 
 
@@ -55,15 +43,12 @@ object FactorDeCompresion{
 }
 //----------------------------SUBCLASE-FOTO
 class Foto(val alto: Int, val ancho: Int) : Publicacion() {
-
     override fun espacioQueOcupa() = ceil(alto * ancho * FactorDeCompresion.coeficiente).toInt()
 }
 //----------------------------SUBCLASE-TEXTO
 class Texto(val contenido: String) : Publicacion() {
-
     override fun espacioQueOcupa() = contenido.length
 }
-
 //----------------------------SUBCLASE-VIDEO
 class Video(val duracion: Int, var calidad: String): Publicacion(){
     var calidadVideo :Calidades = SD(this)
@@ -76,24 +61,37 @@ class Video(val duracion: Int, var calidad: String): Publicacion(){
         }
     }
     fun cambiarCalidad(nuevaCalidad: String){
-        if (nuevaCalidad.toLowerCase() == "sd")       {calidadVideo = SD(this) }
-        if (nuevaCalidad.toLowerCase() == "hd720")    {calidadVideo = HD720(this)}
-        if (nuevaCalidad.toLowerCase() == "hd1080")   {calidadVideo = HD1080(this)}
+        if (nuevaCalidad.toLowerCase() == "sd")     {calidadVideo = SD(this) }
+        if (nuevaCalidad.toLowerCase() == "hd720")  {calidadVideo = HD720(this)}
+        if (nuevaCalidad.toLowerCase() == "hd1080") {calidadVideo = HD1080(this)}
     }
     override fun espacioQueOcupa() = calidadVideo.devolverCalidad()
-    }
-
+}
 //--------calidad abstracta-----------------------------------//
-
 abstract class Calidades(){
     abstract fun devolverCalidad(): Int
 }
-
 //----------------------calidades-en-si---------//
 class SD(var video: Video): Calidades()     { override fun devolverCalidad() = video.duracion }
 class HD720(var video: Video): Calidades()  { override fun devolverCalidad() = video.duracion * 3 }
 class HD1080(var video: Video): Calidades() { override fun devolverCalidad() = video.duracion * 6 }
-//-----------------------------------------------------------//
+
+
+
+
+
+
+
+//----------------------------------PERMISOS-------------------/
+
+
+
+
+
+
+
+
+
 
 
 
