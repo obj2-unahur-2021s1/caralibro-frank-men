8067b1564d365ccd350a55c5aa12ec915eac1099
Daniel Mendez <rdmmendez@hotmail.com>
2021-04-23 13:44:12 -0300

    Agregado de funcionalidad me gusta (parcial)

--- src/main/kotlin/ar/edu/unahur/obj2/caralibro/Usuario.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/caralibro/Usuario.kt
@@ -5,7 +5,7 @@ class Usuario {
     var amigos = mutableListOf<Usuario>()
     var listaDePermitidos = mutableListOf<Usuario>()
     var listaDeExcluidos = mutableListOf<Usuario>()
-    // var registroDeMeGusta: MutableMap<Usuario,Publicacion> = mutableMapOf() es idea no implementada
+    var registroDeMeGusta: MutableMap<Publicacion, Usuario> = mutableMapOf()
 
     fun agregarPublicacion(publicacion: Publicacion) {
         publicaciones.add(publicacion)
@@ -13,19 +13,45 @@ class Usuario {
     fun agregarAmigo(amigo: Usuario) {
         amigos.add(amigo)
     }
+    fun agregarPermitido(amigo: Usuario){
+        check(!listaDePermitidos.contains(amigo)){
+            listaDePermitidos.add((amigo))
+        }
+    }
+    fun agregarExcluido(amigo: Usuario){
+        check(!listaDeExcluidos.contains(amigo)){
+            listaDeExcluidos.add((amigo))
+        }
+    }
     fun espacioDePublicaciones() = publicaciones.sumBy { it.espacioQueOcupa() }
 
     fun puedeDarMeGusta(publicacion: Publicacion, usuario: Usuario) =
         (amigos.contains(usuario) && this.estaPublicacionYTienePermisos(publicacion,usuario )) || usuario==this
 
+    fun meGusta(publicacion: Publicacion, usuario: Usuario) {
+        check(!puedeDarMeGusta(publicacion,usuario)){
+            this.registrarMeGusta(publicacion,usuario)
+        }
+
+    }
+    private fun registrarMeGusta(publicacion: Publicacion, usuario: Usuario) {
+        //TODO verificar en el registro si ya existe clave-valor
+        //registroDeMeGusta[publicacion] = usuario
+    }
+
+
+
+    // validaciones y funciones accesorias
     private fun estaPublicacionYTienePermisos(publicacion: Publicacion, usuario: Usuario) =
         usuario.contiene(publicacion) && this.validarPermiso(usuario, publicacion)
     private fun contiene(publicacion: Publicacion) = publicaciones.contains(publicacion)
 
+    //validaciones de usuario y publicacion
     private fun validarPermiso(usuario: Usuario, publicacion: Publicacion) =
         this.publico(publicacion) || this.soloAmigos(publicacion,usuario) ||
                 this.privadaConListaDePermitidos(publicacion,usuario) || this.publicoConListaDeExcluidos(publicacion,usuario)
 
+    // verificacion del estado de la publicacion
     private fun publico(publicacion: Publicacion) = (publicacion.permisoPublicacion == "Publico")
     private fun soloAmigos(publicacion: Publicacion, usuario: Usuario) =
         (publicacion.permisoPublicacion == "SoloAmigos") && (amigos.contains(usuario))
@@ -38,22 +64,7 @@ class Usuario {
     private fun estaEnListaDeExcluidos(usuario: Usuario) = listaDeExcluidos.contains(usuario)
 
 
-//    fun crearPublicacion(tipoPublicacion: Publicacion){
-//    }
-/*
-    fun puedeVerPublicacion(publicacion: Publicacion): Boolean{
-        // para que un usuario pueda ver una publicacion, tiene que
-        // estar incluido entre los usuarios del permiso de la publicacion.
-        return publicacion.admiteAUsuario(this)
-    }
 
-    fun darMeGusta(publicacion: Publicacion){
-        if (this.puedeVerPublicacion(publicacion)){
-            publicacion.recibirMeGusta(this)
-        }
-    }
-
- */
 
 
 }
