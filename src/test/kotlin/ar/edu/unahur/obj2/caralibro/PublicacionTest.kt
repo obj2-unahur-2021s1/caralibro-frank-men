8cc258d43f06b522b24b2fb3362fc45f27080721
Daniel Mendez <rdmmendez@hotmail.com>
2021-04-27 13:41:40 -0300

    Algunas correcciones

--- src/test/kotlin/ar/edu/unahur/obj2/caralibro/PublicacionTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/caralibro/PublicacionTest.kt
@@ -1,18 +1,20 @@
 package ar.edu.unahur.obj2.caralibro
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.booleans.shouldBeFalse
+import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldNotBeEmpty
 
 class PublicacionTest: DescribeSpec ({
+    val textito = Texto("Hoy me clave un pancho")
+    val fotito = Foto(1280, 720)
 
     describe("Crear Texto") {
-        val textito = Texto("Hoy me clave un pancho")
-        //textito.textoPublicacion.shouldBe("Hoy me clave un pancho")
+       textito.contenido.shouldBe("Hoy me clave un pancho")
     }
     describe("Crear Foto") {
-        val fotito = Foto(1280, 720)
         fotito.espacioQueOcupa().shouldBe(645120)
     }
 
@@ -29,39 +31,39 @@ class PublicacionTest: DescribeSpec ({
             val videitoHD1080 = Video(100, "hd1080")
             videitoHD1080.espacioQueOcupa().shouldBe(600)
         }
-        describe("Cambiar resoluciones") {
-            it("Cambiar video SD a HD720") {
-                val videito = Video(100, "sd")
-                videito.espacioQueOcupa().shouldBe(100)
-                videito.cambiarCalidad("hd720")
-                videito.espacioQueOcupa().shouldBe(300)
-            }
-            it("Cambiar video HD1080 a SD") {
-                val videote = Video(300, "HD1080") //con mayus porque no es case sensitive
-                videote.espacioQueOcupa().shouldBe(1800)
-                videote.cambiarCalidad("sD")
-                videote.espacioQueOcupa().shouldBe(300)
-            }
-            it("Hd720 a SD y despues a 1080") {
-                val videoDeMichis = Video(60, "hD720")
-                videoDeMichis.espacioQueOcupa().shouldBe(180)
-                videoDeMichis.cambiarCalidad("sd")
-                videoDeMichis.espacioQueOcupa().shouldBe(60)
-                videoDeMichis.cambiarCalidad("HD1080")
-                videoDeMichis.espacioQueOcupa().shouldBe(360)
-            }
+    }
+    describe("Cambiar resoluciones") {
+        it("Cambiar video SD a HD720") {
+            val videito = Video(100, "sd")
+            videito.espacioQueOcupa().shouldBe(100)
+            videito.cambiarCalidad("hd720")
+            videito.espacioQueOcupa().shouldBe(300)
         }
-        describe(""){
-            val textito = Texto("Hola amigos") //creo la publicacion.
-            val juancito = Usuario() // Usuario dueño de la publicacion
-            val pepito = Usuario() //Usuario ajeno
-            juancito.agregarPublicacion(textito)
-            textito.cambiarPermiso("solo amigos")
-            juancito.puedeVerPublicacion(textito).shouldBe(true)
-            pepito.puedeVerPublicacion(textito).shouldBe(false)
-            textito.cambiarPermiso("publico")
-            pepito.puedeVerPublicacion(textito).shouldBe(true)
+        it("Cambiar video HD1080 a SD") {
+            val videote = Video(300, "HD1080") //con mayus porque no es case sensitive
+            videote.espacioQueOcupa().shouldBe(1800)
+            videote.cambiarCalidad("sD")
+            videote.espacioQueOcupa().shouldBe(300)
         }
+        it("Hd720 a SD y despues a 1080") {
+            val videoDeMichis = Video(60, "hD720")
+            videoDeMichis.espacioQueOcupa().shouldBe(180)
+            videoDeMichis.cambiarCalidad("sd")
+            videoDeMichis.espacioQueOcupa().shouldBe(60)
+            videoDeMichis.cambiarCalidad("HD1080")
+            videoDeMichis.espacioQueOcupa().shouldBe(360)
+        }
+    }
+    describe("Cambio de permiso"){
+        val textito2 = Texto("Hola amigos") //creo la publicacion.
+        val juancito = Usuario() // Usuario dueño de la publicacion
+        val pepito = Usuario() //Usuario ajeno
+        juancito.agregarPublicacion(textito2)
+        textito2.cambiarPermiso("solo amigos")
+        juancito.puedeVerPublicacion(textito2).shouldBeTrue()
+        pepito.puedeVerPublicacion(textito2).shouldBeFalse()
+        textito2.cambiarPermiso("publico")
+        pepito.puedeVerPublicacion(textito2).shouldBeTrue()
     }
 })
 
